986d901beb345a8b2923990dfc69291006f63448
xrsrke <xariusdrake@hotmail.com>
2023-05-04 11:04:57 +0700

    add forward pass of RowParallelLinear and ColumnParallelLinea

--- minitron/model/layers.py
+++ minitron/model/layers.py
@@ -2,41 +2,65 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from .utils import get_model_parallel_world_size
-from .mappings import (
-    copy_to_model_parallel_region,
-    gather_from_model_parallel_region
-)
+# from .utils import get_model_parallel_world_size
+# from .mappings import (
+#     copy_to_model_parallel_region,
+#     gather_from_model_parallel_region
+# )
 
 
-class ColumnParallelLinear(nn.Module):
-    def __init__(
-        self,
-        input_size,
-        output_size,
-    ):
+class ColumnParallelLinear(torch.nn.Module):
+    def __init__(self, input_size: int, output_size: int, num_partitions: int):
         super().__init__()
-        world_size = get_model_parallel_world_size()
-
         self.input_size = input_size
         self.output_size = output_size
-        self.output_size_per_partition = output_size // world_size
+        self.output_size_per_partition = output_size // num_partitions
 
-        self.weight = nn.Parameter(
-            torch.empty(
-                self.output_size_per_partition,
-                self.input_size,
-                device=torch.cuda.current_device()
-            )
-        )
-        self.bias = nn.Parameter(
-            torch.empty(
-                self.output_size_per_partition,
-                device=torch.cuda.current_device()
-            )
-        )
+        self.weight = nn.Parameter(torch.empty(
+            self.output_size_per_partition,
+            self.input_size
+        ))
+        self.bias = nn.Parameter(torch.empty(
+            self.output_size_per_partition
+        ))
 
-    def forward(self, input):
-        input_parallel = copy_to_model_parallel_region(input)
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output_parallel = F.linear(input, self.weight, self.bias)
+        world_size = torch.distributed.get_world_size()
+        outputs = [torch.empty_like(output_parallel) for _ in range(world_size)]
+        torch.distributed.all_gather(outputs, output_parallel)
+
+        outputs = torch.cat(outputs, dim=-1).contiguous()
+        return outputs
+
+
+class RowParallelLinear(nn.Module):
+    def __init__(self, input_size: int, output_size: int):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+
+        world_size = torch.distributed.get_world_size()
+        self.input_size_per_patrition = input_size // world_size
+        self.weight = nn.Parameter(torch.empty(
+            self.output_size,
+            self.input_size_per_patrition
+        ))
+        self.bias = nn.Parameter(torch.empty(
+            self.output_size
+        ))
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        # split the input
+        last_dim_size = input.shape[-1]
+        world_size = torch.distributed.get_world_size()
+        dim_per_patrition = last_dim_size // world_size
+        input_chunks = torch.split(input, dim_per_patrition, dim=-1)
+
+        rank = torch.distributed.get_rank()
+        input_parallel = input_chunks[rank]
         output_parallel = F.linear(input_parallel, self.weight, self.bias)
-        output = gather_from_model_parallel_region(output_parallel)
+
+        torch.distributed.all_reduce(output_parallel)
+
+        return output_parallel
